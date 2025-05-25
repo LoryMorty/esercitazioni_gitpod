43d80102facdff69e1a9c6b7f33f7d4c4b94debd
Lorenzo Mortillaro <mortillaro.lorenzo@iisgalvanimi.edu.it>
2025-05-25 17:53:56 +0000

    abbiamo modificato il ts dell' article component in modo tale da usare la classe appena creata nel models

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit, HostBinding  } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model'; // Importa la classe Article
 
 @Component({
-  selector: 'app-article',
-  imports: [],
-  templateUrl: './article.component.html',
-  styleUrl: './article.component.css'
+  selector: 'app-article',                     // Tag usato nel template per richiamare questo componente
+  templateUrl: './article.component.html',     // Template HTML associato
+  styleUrls: ['./article.component.css']       // Foglio di stile
 })
-export class ArticleComponent {
-  @HostBinding('attr.class') cssClass = 'card';
-  votes: number;
-  title: string;
-  link: string;
-  constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
-  }
-  voteUp() {
-    this.votes += 1;
-    return false;
-  }
-  voteDown() {
-    this.votes -= 1;
-    return false;
-  }
-  ngOnInit() {}
-}
+export class ArticleComponent implements OnInit {
+  // Associa la classe CSS "card" al div root del componente
+  @HostBinding('class') cssClass = 'card';
 
+  article: Article;  // Proprietà che rappresenta un singolo articolo
+
+  constructor() {
+    // Inizializza l’articolo con dati fittizi
+    this.article = new Article('Angular 17', 'https://angular.io', 10);
+  }
+
+  // Aumenta i voti dell’articolo
+  voteUp(): boolean {
+    this.article.voteUp(); // Usa il metodo del model
+    return false;          // Blocca il comportamento predefinito del click (es: ricarica)
+  }
+
+  // Diminuisce i voti dell’articolo
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit(): void {}  // Metodo chiamato alla creazione del componente (hook del ciclo di vita)
+}
